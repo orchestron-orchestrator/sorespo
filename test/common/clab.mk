1e841495855a6579d6c8a53a279edbc600d4470d
Marko Zagožen <marko.zagozen@gmail.com>
2025-03-06 13:41:13 +0000

    Export IMAGE_PATH for local containerlab

--- test/common/clab.mk
+++ test/common/clab.mk
@@ -1,6 +1,6 @@
 PROJECT_DIR:=$(realpath $(dir $(lastword $(MAKEFILE_LIST)))/../../)
 # Set this env var to empty string if you have local cRPD, XRd container images
-IMAGE_PATH?=registry.devops.telekom.de/containerlab/containerlab/
+export IMAGE_PATH?=registry.devops.telekom.de/containerlab/containerlab/
 
 ifeq (true,$(REMOTE_CONTAINERS))
 CLAB_BIN:=containerlab
