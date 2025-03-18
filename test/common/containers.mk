cf29560a31e1b2b0e571eac98a84a24a74024211
Marko Zagožen <marko.zagozen@gmail.com>
2025-03-18 08:25:02 +0100

    Platform test for all

--- test/common/containers.mk
+++ test/common/containers.mk
@@ -15,3 +15,27 @@ $(addprefix platform-cli-,$(ROUTERS_XR)):
 
 $(addprefix platform-cli-,$(ROUTERS_CRPD)):
 	docker exec -it $(TESTENV)-$(subst platform-cli-,,$@) cli
+
+test:: test-ping
+
+# These test-ping-% recipes will "install" themselves for all PE routers too.
+# Pretty sure pinging the customer loopback from the default VRF in core does
+# not work, but these are internal helpers anyway ...
+$(addprefix test-ping-,$(ROUTERS_CRPD)):
+# brew install coreutils on MacOS
+	timeout --foreground 10s bash -c "until docker exec -t $(TESTENV)-$(@:test-ping-%=%) ping -c 1 -W 1 $(IP); do sleep 1; done"
+
+$(addprefix test-ping-,$(ROUTERS_XR)):
+# brew install coreutils on MacOS
+	timeout --foreground 10s bash -c "until docker exec -t $(TESTENV)-$(@:test-ping-%=%) ip netns exec global-vrf ping -c 1 -W 1 $(IP); do sleep 1; done"
+
+.PHONY: test-ping
+test-ping:
+# Do a full mesh of ping between all cust-X routers loopbacks
+	@set -e; for i in 1 2 3 4; do \
+		for j in 1 2 3 4; do \
+			if [ $$i -ne $$j ]; then \
+				$(MAKE) test-ping-cust-$$i IP=10.200.1.$$j; \
+			fi; \
+		done; \
+	done
--- test/common/notconf.mk
+++ test/common/notconf.mk
@@ -9,3 +9,7 @@ $(addprefix platform-wait-,$(ROUTERS_XR) $(ROUTERS_CRPD)):
 $(addprefix platform-cli-,$(ROUTERS_XR) $(ROUTERS_CRPD)):
 	@echo "Executing netopeer2-cli on $(subst platform-cli-,,$@). To start, type 'connect -u admin'"
 	@docker exec -it $(TESTENV)-$(subst platform-cli-,,$@) netopeer2-cli
+
+test::
+	@echo "TODO: notconf"
+# check config??
