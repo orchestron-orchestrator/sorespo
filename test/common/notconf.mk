5fdc18d0902de3f4b2f92327856843fa542b1403
Marko Zagožen <marko.zagozen@gmail.com>
2025-03-20 09:37:21 +0100

    Include NETCONF server test in 'wait'
    Ideally this would be done in Orchestron - its device manager should
    make attempts to connect to a device periodically. Until this is
    implemented let's wait until the NETCONF servers are ready before
    starting Orchestron.

--- test/common/notconf.mk
+++ test/common/notconf.mk
@@ -3,6 +3,7 @@
 $(addprefix platform-wait-,$(ROUTERS_XR) $(ROUTERS_CRPD)):
 # Wait for "Listening on 0.0.0.0:830 for SSH connections" to appear in the container logs
 	timeout --foreground $(WAIT) bash -c "until docker logs $(TESTENV)-$(@:platform-wait-%=%) 2>&1 | grep -q 'Listening on 0.0.0.0:830 for SSH connections'; do sleep 1; done"
+	docker run $(INTERACTIVE) --rm --network container:$(TESTENV)-otron ghcr.io/notconf/notconf:debug netconf-console2 --host $(@:platform-wait-%=%) --port 830 --user clab --pass clab@123 --hello
 
 .PHONY: $(addprefix platform-cli-,$(ROUTERS_XR) $(ROUTERS_CRPD))
 
