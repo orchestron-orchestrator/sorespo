eb7958b05c176b40f4e5c431c480dd44dbf57bf0
Kris Lambrechts <kris@netedge.plus>
2026-04-10 16:01:21 +0000

    Monitor interface traffic on IOS-XR
    Add a monitor-traffic option on the backbone-link.
    When enabled, the backbone-interface descriptions
    will include Rx PPS infirmation.
    
    The current implementation supports IOS-XR only.

--- test/common/quicklab.mk
+++ test/common/quicklab.mk
@@ -73,6 +73,14 @@ configure-tmf640:
 	$(MAKE) send-config-tmf640 FILE="netinfra-tmf640-backbone-link-fra-lju.json"
 	$(MAKE) send-config-tmf640 FILE="netinfra-tmf640-backbone-link-sto-lju.json"
 
+.PHONY: monitor-traffic-enable
+monitor-traffic-enable:
+	$(MAKE) send-config-wait FILE="monitor-traffic-enable.xml"
+
+.PHONY: monitor-traffic-disable
+monitor-traffic-disable:
+	$(MAKE) send-config-wait FILE="monitor-traffic-disable.xml"
+
 .PHONY: tutorial
 tutorial:
 	$(MAKE) -C ../../ download-release
